a8fa5deed4c5fd0034d0fa545bd244645b979a32
Rahul Bharati <rahulrbharati@gmail.com>
2022-01-03 23:50:53 +0530

    1. setup hardhat and next app on polygon network 2. Created NFT smart contract 3. Working on creating NFRMarker smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon_mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
